e17662ca98f593de4972fc9b0c8d234def5b7b0e
Abdullah Demirer <abdullah_demirer@outlook.com>
2021-05-13 17:47:08 +0300

    Result has added

---
+++ Core/Utilities/Results/DataErrorResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    class DataErrorResult<T>:DataResult<T>
+    {
+        public DataErrorResult(T data, string message):base(data,false,message)
+        {
+
+        }
+        public DataErrorResult(T data) : base(data, false)
+        {
+
+        }
+    }
+}
